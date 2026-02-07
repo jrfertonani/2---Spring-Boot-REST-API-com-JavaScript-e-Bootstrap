3318bf7d71a7f6bd3baf0f19feb8a1350a81c060
Ademir <ademirjuniorfertonani@gmail.com>
2026-02-07 19:44:20 -0300

    listar usuarios

---
+++ src/main/java/Java/controller/GreetingsController.java
@@ -0,0 +1,38 @@
+package Java.controller;
+
+import Java.model.Usuario;
+import Java.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class GreetingsController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @RequestMapping(value = "/{nome}")
+    public String ola(@PathVariable String nome){
+        Usuario usuario = new Usuario();
+        usuario.setNome(nome);
+        usuarioRepository.save(usuario);
+        return "Ola" + nome;
+    };
+
+    @GetMapping(value = "/listatodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listarUsuario() {
+
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+
+    }
+
+}
---
+++ src/main/java/Java/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package Java.repository;
+
+import Java.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+
+}
